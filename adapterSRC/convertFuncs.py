4c31bb18d0ef6c9bf82717dcb8524d0e51fe6df3
NotTimm <gamer.tdogtim@gmail.com>
2023-04-30 15:00:56 -0500

    added another converter

--- adapterSRC/convertFuncs.py
+++ adapterSRC/convertFuncs.py
@@ -1,4 +1,4 @@
-import json, xmltodict, sys
+import json, xmltodict, sys, csv
 
 def xml2json(driveLocation):
     with open(driveLocation) as xml:
@@ -6,6 +6,16 @@ def xml2json(driveLocation):
         jsonData = json.dumps(data)
         with open(driveLocation[:-3]+"json", "w") as jsonOut:
             jsonOut.write(jsonData)
+
+def csv2json(driveLocation):
+    out = {}
+    with open(driveLocation) as csvIn:
+        data = csv.DictReader(csvIn)
+        for rows in data:
+            key = rows['No']
+            out[key] = rows
+    with open(driveLocation[:-3]+"json", "w") as jsonOut:
+        jsonOut.write(json.dumps(data, indent=4))
 
 sys.modules[__name__] = xml2json
 
